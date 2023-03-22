352bd4c15597f66689d4307727e89bb1e2f2a541
Nicolas Vercaigne <nicolas.vercaigne@skyline.be>
2023-03-22 14:32:46 +0100

    Add project files.

---
+++ EmpowerNVC2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmpowerNVC2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("EmpowerNVC2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8BFD722E-5D06-4789-B930-BC6063B8F7DB")]
+[assembly: AssemblyVersion("1.0.0.0")]
